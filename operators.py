e56f2b3f9c7c8539fa6f0482e60b7263329770f3
Przemek <przemeksienkiewicz@gmail.com>
2021-04-11 09:40:34 +0200

    Add operators file

---
+++ operators.py
@@ -0,0 +1,25 @@
+v1 = float(input("First value:"))
+v2 = float(input("Second value:"))
+
+print(v1, "+", v2, "=", v1+v2)
+print(v1, "-", v2, "=", v1-v2)
+print(v1, "*", v2, "=", v1*v2)
+print(v1, "**", v2, "=", v1**v2)
+if v2 != 0:
+    print(v1, "/", v2, "=", v1/v2)
+    print(v1, "%", v2, "=", v1%v2)
+else:
+    print("Second value is 0!")
+
+''' Operatory porównania
+ ==
+ !=
+ >
+ >=
+ <
+ <=
+    Operatory przypisania
+ =
+ += (x=x+4 <=> x+=4)
+ -= *= /= %= **=
+'''

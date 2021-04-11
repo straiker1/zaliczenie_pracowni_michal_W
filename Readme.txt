054f4083a5a6d1bb7339e77cb6a1ca160717081f
Przemek <przemeksienkiewicz@gmail.com>
2021-04-11 09:21:32 +0200

    First

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Informacje dla developera:
+- Projekt w Python 3.8
+- IDE PyCharm
---
+++ print.py
@@ -0,0 +1,21 @@
+''' komenarz
+wielo
+liniowy '''
+print("Line 0")
+print("Line 1\nLine 2 \t\t \"text\"")
+# v1 = 1
+# v2 = '1'
+# v3 = 1.0
+# v4 = 'text'
+# v5 = 4.5
+# v6 = True
+# print("v1 -", v1, "-", type(v1))
+# print("v2 -", v2, "-", type(v2))
+# print("v3 -", v3, "-", type(v3))
+# v4 = 2021
+# print("v4 -", v4, "-", type(v4))
+# print("v5 -", v5, "-", type(v5))
+# print("v6 -", v6, "-", type(v6))
+
+str1 = input("Podaj rok urodzenia:")
+print("Wiek: ", 2021 - int(str1))

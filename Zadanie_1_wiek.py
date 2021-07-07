124a3cd6c2bc8288ddcff17a2cf5f7b8ad59b530
straiker1 <87087116+straiker1@users.noreply.github.com>
2021-07-07 21:54:40 +0200

    Add files via upload

---
+++ Zadanie_1_wiek.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import datetime
+
+currentDate = datetime.date.today()
+aktRok = currentDate.year
+rokur = None
+wiek = None
+
+
+rokur = int(input('w ktorym roku sie urodziles? '))
+
+if rokur > aktRok:
+    print('podales bledny rok urodzenia')
+else:
+    wiek = aktRok - rokur
+    print( 'masz', wiek, 'lat' )
+    if wiek >= 18:
+        print('jestes pelnoletni')
+    else:
+        print('jestes niepelnoletni')
